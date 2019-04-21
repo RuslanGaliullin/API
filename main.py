885d5cf0ff17bc0339e4eea19c8d96382492d361
Rusik <rusgaliul00@gmail.com>
2019-04-21 18:43:10 +0300

    api whether translation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Импортируем необходимые классы.
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+import requests
 import random
 
 reply_keyboard = [['/dice'],
@@ -19,12 +20,58 @@ reply_keyboard_close = [['/close']]
 markup_close = ReplyKeyboardMarkup(reply_keyboard_close, one_time_keyboard=False)
 
 
-def start(bot, update):
+class Helper:
+    def get_coords(self, adress):
+        try:
+            address = adress
+            # Собираем параметры для запроса к StaticMapsAPI:
+            map_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+            map_params = {"geocode": address, "format": "json"}
+            response_metro = requests.get(map_api_server, params=map_params)
+            json_response = response_metro.json()
+
+            # Получаем первый топоним из ответа геокодера.
+            # Согласно описанию ответа, он находится по следующему пути:
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            # Полный адрес топонима:
+            toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+            # Координаты центра топонима:
+            toponym_coodrinates = toponym["Point"]["pos"]
+            return toponym_coodrinates  # строка
+        except:
+            print('ошибочка')
+
+    def get_language(self, lang):
+        if lang in all.keys():
+            all = {'английский': 'en', 'русский': 'ru', 'абхазский': 'ab', 'арабский'
+            : 'ar', 'азербайджанский': 'az', 'армянский': 'hy', 'башкирский': 'ba', 'белорусский': 'be', 'болгарский': 'bg',
+                   'венгерский': 'hu', 'вьетнамский': 'vi', 'грузинский': 'ka', 'датский': 'da', 'иврит': 'he',
+                   'испанский': 'es', 'итальянский': 'it', 'немецкий': 'de', 'корейский': 'ko', 'японский': 'ja',
+                   'португальский': 'pt'}
+            return all[lang]
+        return 'Я не знаю такой язык'
+
+
+helpp = Helper()
+
+
+def start(bot, update, city):
+    # погода
+    api_weather = 'https://api.weather.yandex.ru/v1/informers?'
+    cords = helpp.get_coords(city).split()
+    params = {'lat': cords[0], 'lon': cords[1], 'lang': 'ru_RU'}
+    response = requests.get(api_weather, params=params)
     update.message.reply_text("Я Бот-помощник для игр. Что вам нужно?",
                               reply_markup=markup)
 
 
-def dice(bot, update):
+def dice(bot, update, phrase, lang):
+    # переводчик
+    api_translation = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
+    params = {'key': 'trnsl.1.1.20190421T150726Z.fe7b6a8c58b8788e.422cda1d99bc4cbed5fd2685e0f4f423a6ec5eda',
+              'text': phrase, 'lang': helpp.get_language(lang)+'_'+helpp.get_language(lang).upper()}
+    response = requests.get(api_translation, params=params)
     update.message.reply_text("Вы выбрали dice.",
                               reply_markup=markup_dice)
 
@@ -59,7 +106,6 @@ def close(bot, update, chat_data):
         chat_data['job'].schedule_removal()
         # и очищаем пользовательские данные
         del chat_data['job']
-
     update.message.reply_text('Таймер сброшен', reply_markup=markup_timer)
 
 
@@ -73,10 +119,8 @@ def tri_sec(bot, update, job_queue, chat_data):
     # (будет доступен через job.context)
     delay = 30  # секунд
     job = job_queue.run_once(task30, delay, context=update.message.chat_id)
-
     # Запоминаем в пользовательских данных созданную задачу.
     chat_data['job'] = job
-
     # Присылаем сообщение о том, что всё получилось.
     update.message.reply_text('засек 30 секунд', reply_markup=markup_close)
 
@@ -91,10 +135,8 @@ def min_sec(bot, update, job_queue, chat_data):
     # (будет доступен через job.context)
     delay = 60  # секунд
     job = job_queue.run_once(task30, delay, context=update.message.chat_id)
-
     # Запоминаем в пользовательских данных созданную задачу.
     chat_data['job'] = job
-
     # Присылаем сообщение о том, что всё получилось.
     update.message.reply_text('засек минуту', reply_markup=markup_close)
 
@@ -109,10 +151,8 @@ def five_min_sec(bot, update, job_queue, chat_data):
     # (будет доступен через job.context)
     delay = 300  # секунд
     job = job_queue.run_once(task300, delay, context=update.message.chat_id)
-
     # Запоминаем в пользовательских данных созданную задачу.
     chat_data['job'] = job
-
     # Присылаем сообщение о том, что всё получилось.
     update.message.reply_text('засек 5 минут', reply_markup=markup_close)
 
@@ -121,16 +161,13 @@ def main():
     # Создаём объект updater. Вместо слова "TOKEN" надо разместить
     # полученный от @BotFather токен
     updater = Updater("879379687:AAHXkIj5tPSCi7zlJaGH6fHAhoz_y-q88EU")
-
     # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
-
     # Создаём обработчик сообщений типа Filters.text
     # из описанной выше функции echo()
     # После регистрации обработчика в диспетчере эта функция
     # будет вызываться при получении сообщения с типом "текст",
     # т.е. текстовых сообщений.
-
     # Зарегистрируем их в диспетчере.
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("dice", dice))
@@ -146,10 +183,8 @@ def main():
     dp.add_handler(CommandHandler("5min", five_min_sec,
                                   pass_job_queue=True, pass_chat_data=True))
     dp.add_handler(CommandHandler("close", close, pass_chat_data=True))
-
     # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
-
     # Ждём завершения приложения.
     # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
     updater.idle()
