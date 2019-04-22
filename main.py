3c9e0f7610c74060c5ad36e4a49b5dbc5469ce32
ProgrammerOtBoga <fatahoff.georgy@yandex.ru>
2019-04-22 20:54:53 +0300

    rename

--- main.py
+++ main.py
@@ -4,20 +4,9 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import requests
 import random
 
-reply_keyboard = [['/dice'],
-                  ['/timer']]
+reply_keyboard = [['/zontik', '/perevod', '/pogoda'],
+                  ['/kartinka', '/dobratsa', '/apteka']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-
-reply_keyboard_dice = [['/RollOneDie', '/RollTwoDie'],
-                       ['/RollMegaDie', '/back']]
-markup_dice = ReplyKeyboardMarkup(reply_keyboard_dice, one_time_keyboard=False)
-
-reply_keyboard_timer = [['/30sec', '/minute'],
-                        ['/5min', '/back']]
-markup_timer = ReplyKeyboardMarkup(reply_keyboard_timer, one_time_keyboard=False)
-
-reply_keyboard_close = [['/close']]
-markup_close = ReplyKeyboardMarkup(reply_keyboard_close, one_time_keyboard=False)
 
 
 class Helper:
@@ -45,7 +34,8 @@ class Helper:
     def get_language(self, lang):
         if lang in all.keys():
             all = {'английский': 'en', 'русский': 'ru', 'абхазский': 'ab', 'арабский'
-            : 'ar', 'азербайджанский': 'az', 'армянский': 'hy', 'башкирский': 'ba', 'белорусский': 'be', 'болгарский': 'bg',
+            : 'ar', 'азербайджанский': 'az', 'армянский': 'hy', 'башкирский': 'ba', 'белорусский': 'be',
+                   'болгарский': 'bg',
                    'венгерский': 'hu', 'вьетнамский': 'vi', 'грузинский': 'ka', 'датский': 'da', 'иврит': 'he',
                    'испанский': 'es', 'итальянский': 'it', 'немецкий': 'de', 'корейский': 'ko', 'японский': 'ja',
                    'португальский': 'pt'}
@@ -58,103 +48,36 @@ helpp = Helper()
 
 def start(bot, update, city):
     # погода
-    api_weather = 'https://api.weather.yandex.ru/v1/informers'
+    api_weather = 'https://api.weather.yandex.ru/v1/informers?'
     cords = helpp.get_coords(city).split()
     params = {'lat': cords[0], 'lon': cords[1], 'lang': 'ru_RU'}
     response = requests.get(api_weather, params=params)
-    update.message.reply_text("Я Бот-помощник для игр. Что вам нужно?",
+    update.message.reply_text("Я Бот-помощник для ДЭБИЛ. Что вам нужно?",
                               reply_markup=markup)
 
 
-def dice(bot, update, phrase, lang):
-    # переводчик
-    api_translation = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-    params = {'key': 'trnsl.1.1.20190421T150726Z.fe7b6a8c58b8788e.422cda1d99bc4cbed5fd2685e0f4f423a6ec5eda',
-              'text': phrase, 'lang': helpp.get_language(lang)+'_'+helpp.get_language(lang).upper()}
-    response = requests.get(api_translation, params=params)
-    update.message.reply_text(
-        "\n\n".join([response.json()["text"][0]]))
+def zontik(bot, update):
+    pass
 
 
-def RollOneDie(bot, update):
-    update.message.reply_text(random.randint(1, 6))
+def perevod(bot, update):
+    pass
 
 
-def RollTwoDie(bot, update):
-    update.message.reply_text('{0} {1}'.format(random.randint(1, 6), random.randint(1, 6)))
+def pogoda(bot, update):
+    pass
 
 
-def RollMegaDie(bot, update):
-    update.message.reply_text(random.randint(1, 20))
+def kartinka(bot, update):
+    pass
 
 
-def back(bot, update):
-    update.message.reply_text("Вы вышли в главное меню.", reply_markup=markup)
+def dobratsa(bot, update):
+    pass
 
 
-def timer(bot, update):
-    update.message.reply_text("Вы выбрали timer.",
-                              reply_markup=markup_timer)
-
-
-def close(bot, update, chat_data):
-    # Проверяем, что задача ставилась
-    # (вот зачем нужно было ее записать в chat_data).
-    print('ssds')
-    if 'job' in chat_data:
-        # планируем удаление задачи (выполнется, когда будет возможность)
-        chat_data['job'].schedule_removal()
-        # и очищаем пользовательские данные
-        del chat_data['job']
-    update.message.reply_text('Таймер сброшен', reply_markup=markup_timer)
-
-
-def task30(bot, job):
-    bot.send_message(job.context, text='30 секунд истекло')
-
-
-def tri_sec(bot, update, job_queue, chat_data):
-    # создаём задачу task в очереди job_queue через 20 секунд
-    # передаём ей идентификатор текущего чата
-    # (будет доступен через job.context)
-    delay = 30  # секунд
-    job = job_queue.run_once(task30, delay, context=update.message.chat_id)
-    # Запоминаем в пользовательских данных созданную задачу.
-    chat_data['job'] = job
-    # Присылаем сообщение о том, что всё получилось.
-    update.message.reply_text('засек 30 секунд', reply_markup=markup_close)
-
-
-def task60(bot, job):
-    bot.send_message(job.context, text='минута истекла')
-
-
-def min_sec(bot, update, job_queue, chat_data):
-    # создаём задачу task в очереди job_queue через 20 секунд
-    # передаём ей идентификатор текущего чата
-    # (будет доступен через job.context)
-    delay = 60  # секунд
-    job = job_queue.run_once(task30, delay, context=update.message.chat_id)
-    # Запоминаем в пользовательских данных созданную задачу.
-    chat_data['job'] = job
-    # Присылаем сообщение о том, что всё получилось.
-    update.message.reply_text('засек минуту', reply_markup=markup_close)
-
-
-def task300(bot, job):
-    bot.send_message(job.context, text='5 минут истекло')
-
-
-def five_min_sec(bot, update, job_queue, chat_data):
-    # создаём задачу task в очереди job_queue через 20 секунд
-    # передаём ей идентификатор текущего чата
-    # (будет доступен через job.context)
-    delay = 300  # секунд
-    job = job_queue.run_once(task300, delay, context=update.message.chat_id)
-    # Запоминаем в пользовательских данных созданную задачу.
-    chat_data['job'] = job
-    # Присылаем сообщение о том, что всё получилось.
-    update.message.reply_text('засек 5 минут', reply_markup=markup_close)
+def apteka(bot, update):
+    pass
 
 
 def main():
@@ -170,19 +93,12 @@ def main():
     # т.е. текстовых сообщений.
     # Зарегистрируем их в диспетчере.
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("dice", dice))
-    dp.add_handler(CommandHandler("RollOneDie", RollOneDie))
-    dp.add_handler(CommandHandler("RollTwoDie", RollTwoDie))
-    dp.add_handler(CommandHandler("RollMegaDie", RollMegaDie))
-    dp.add_handler(CommandHandler("back", back))
-    dp.add_handler(CommandHandler("timer", timer))
-    dp.add_handler(CommandHandler("30sec", tri_sec,
-                                  pass_job_queue=True, pass_chat_data=True))
-    dp.add_handler(CommandHandler("minute", min_sec,
-                                  pass_job_queue=True, pass_chat_data=True))
-    dp.add_handler(CommandHandler("5min", five_min_sec,
-                                  pass_job_queue=True, pass_chat_data=True))
-    dp.add_handler(CommandHandler("close", close, pass_chat_data=True))
+    dp.add_handler(CommandHandler("zontik", zontik))
+    dp.add_handler(CommandHandler("perevod", perevod))
+    dp.add_handler(CommandHandler("pogoda", pogoda))
+    dp.add_handler(CommandHandler("kartinka", kartinka))
+    dp.add_handler(CommandHandler("dobratsa", dobratsa))
+    dp.add_handler(CommandHandler("apteka", apteka))
     # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
     # Ждём завершения приложения.
